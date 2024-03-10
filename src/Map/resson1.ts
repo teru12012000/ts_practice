e4d3e271358babe471150401c1e504fbdfebfa4a
teru12012000 <sion2769@gmail.com>
2024-03-10 19:54:50 +0900

    keep: keeep

---
+++ src/Map/resson1.ts
@@ -0,0 +1,9 @@
+const map1 = new Map<string, number>()
+
+map1.set("a", 1)
+
+console.log(map1)
+
+const valueA = map1.get("a")
+
+console.log(valueA)
---
+++ src/Map/resson2.ts
@@ -0,0 +1,23 @@
+type dataType = {
+    id: string
+    name: string
+    age: number
+}
+
+const data: dataType[] = [
+    {
+        id: "ghuraghaui",
+        name: "Sato",
+        age: 10
+    },
+    {
+        id: "harjiohajoirhjao",
+        name: "Suzuki",
+        age: 8
+    },
+    {
+        id: "gjarogjaigjaoibamoia",
+        name: "Yamada",
+        age: 30
+    }
+]
